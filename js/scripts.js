464148b3a9071f277590c5a48acb9a495304c6c8
Justin Roller and Noah Medoff <noah@>
2018-03-29 09:53:42 -0700

    initial commit. xonvert numbers to numerals using romanize function

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function romanize(num){
+  var result = "";
+  var decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var numerals = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  for (var i=0; i < decimals.length; i++){
+    while (num % decimals[i] < num){
+      result += numerals[i]
+
+      num -= decimals[i];
+    }
+  }
+  return result;
+};
